0106271948e7930cc48d5778262e7f592887771a
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-08-15 17:03:33 +0200

    Use the FunderMaps taskrunner

--- scripts/refresh_models.py
+++ scripts/refresh_models.py
@@ -4,24 +4,30 @@ import logging
 import colorlog
 
 from fundermapssdk import FunderMapsSDK
-from fundermapssdk.config import DatabaseConfig
+from fundermapssdk.config import DatabaseConfig, S3Config
 from fundermapssdk.util import find_config
 
 
 logger = logging.getLogger("refresh_models")
 
+task_registry = {}
+task_registry_post = {}
 
-async def run(config):
-    db_config = DatabaseConfig(
-        database=config.get("database", "database"),
-        host=config.get("database", "host"),
-        user=config.get("database", "username"),
-        password=config.get("database", "password"),
-        port=config.getint("database", "port"),
-    )
 
-    fundermaps = FunderMapsSDK(db_config=db_config)
+def fundermaps_task(func):
+    """Decorator to register a function."""
+    task_registry[func.__name__] = func
+    return func
 
+
+def fundermaps_task_post(func):
+    """Decorator to register a function."""
+    task_registry_post[func.__name__] = func
+    return func
+
+
+@fundermaps_task
+async def run(fundermaps: FunderMapsSDK):
     with fundermaps.db as db:
         # TODO: Check if enough data has changed to refresh models
 
@@ -62,9 +68,35 @@ if __name__ == "__main__":
     config = find_config()
 
     try:
+        db_config = DatabaseConfig(
+            database=config.get("database", "database"),
+            host=config.get("database", "host"),
+            user=config.get("database", "username"),
+            password=config.get("database", "password"),
+            port=config.getint("database", "port"),
+        )
+        s3_config = S3Config(
+            access_key=config.get("s3", "access_key"),
+            secret_key=config.get("s3", "secret_key"),
+            service_uri=config.get("s3", "service_uri"),
+            bucket=config.get("s3", "bucket"),
+        )
+        fundermaps = FunderMapsSDK(db_config=db_config, s3_config=s3_config)
+
+        async def run_tasks():
+            try:
+                for task_name, task_func in task_registry.items():
+                    logger.debug(f"Running task '{task_name}'")
+                    await task_func(fundermaps)
+            finally:
+                for task_name, task_func in task_registry_post.items():
+                    logger.debug(f"Running post task '{task_name}'")
+                    await task_func(fundermaps)
+
         logger.info("Starting 'refresh_models'")
-        asyncio.run(run(config))
+        asyncio.run(run_tasks())
         logger.info("Finished 'refresh_models'")
+
     except Exception as e:
         logger.error("An error occurred", exc_info=e)
         sys.exit(1)
