af774cdda0be3ea15175f1ff88a2e960ab333f49
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-08-15 13:22:03 +0200

    Find the config file

--- fundermapssdk/util.py
+++ fundermapssdk/util.py
@@ -2,6 +2,8 @@ import os
 import glob
 import httpx
 
+from configparser import ConfigParser
+
 
 async def http_download_file(url, dest_path):
     if os.path.exists(dest_path):
@@ -20,3 +22,31 @@ def remove_files(directory, extension=".gpkg"):
 
     for file_path in files:
         os.remove(file_path)
+
+
+def find_config() -> ConfigParser:
+    """
+    Finds and reads the configuration file.
+
+    Returns:
+        ConfigParser: The parsed configuration object.
+
+    Raises:
+        FileNotFoundError: If no configuration file is found in the specified paths.
+    """
+
+    config = ConfigParser()
+
+    config_paths = [
+        "/etc/fundermaps/config.ini",
+        "./config.ini",
+    ]
+
+    for path in config_paths:
+        if os.path.exists(path):
+            config.read(path)
+            break
+    else:
+        raise FileNotFoundError("No configuration file found in the specified paths.")
+
+    return config
