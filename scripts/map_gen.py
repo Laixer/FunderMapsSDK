73217e698702cc39ecaeea2ce529dce6d58f8a9e
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-08-16 13:58:07 +0200

    Code cleanup

--- scripts/map_gen.py
+++ scripts/map_gen.py
@@ -1,31 +1,12 @@
 import os
-import sys
 import logging
-import asyncio
-import colorlog
 
 from fundermapssdk import FunderMapsSDK
-from fundermapssdk.config import DatabaseConfig, S3Config
 from fundermapssdk.tippecanoe import tippecanoe
-from fundermapssdk.util import find_config, remove_files
+from fundermapssdk import util, app
 
 
 logger = logging.getLogger("map_gen")
-
-task_registry = {}
-task_registry_post = {}
-
-
-def fundermaps_task(func):
-    """Decorator to register a function."""
-    task_registry[func.__name__] = func
-    return func
-
-
-def fundermaps_task_post(func):
-    """Decorator to register a function."""
-    task_registry_post[func.__name__] = func
-    return func
 
 
 class TileBundle:
@@ -105,7 +86,7 @@ async def process_tileset(fundermaps: FunderMapsSDK, tileset: TileBundle):
                 )
 
 
-@fundermaps_task
+@app.fundermaps_task
 async def run(fundermaps: FunderMapsSDK):
     tile_bundles = [
         # TileBundle("Analysis Foundation", "analysis_foundation", 12, 16),
@@ -127,55 +108,8 @@ async def run(fundermaps: FunderMapsSDK):
         await process_tileset(fundermaps, tileset)
 
 
-@fundermaps_task_post
+@app.fundermaps_task_post
 async def run_post(fundermaps: FunderMapsSDK):
-    remove_files(".", extension=".gpkg")
-    remove_files(".", extension=".geojson")
-
-
-if __name__ == "__main__":
-    handler = colorlog.StreamHandler()
-    handler.setFormatter(
-        colorlog.ColoredFormatter("%(log_color)s%(levelname)-8s %(name)s : %(message)s")
-    )
-
-    logging.basicConfig(
-        level=logging.INFO,
-        handlers=[handler],
-    )
-
-    config = find_config()
-
-    try:
-        db_config = DatabaseConfig(
-            database=config.get("database", "database"),
-            host=config.get("database", "host"),
-            user=config.get("database", "username"),
-            password=config.get("database", "password"),
-            port=config.getint("database", "port"),
-        )
-        s3_config = S3Config(
-            access_key=config.get("s3", "access_key"),
-            secret_key=config.get("s3", "secret_key"),
-            service_uri=config.get("s3", "service_uri"),
-            bucket=config.get("s3", "bucket"),
-        )
-        fundermaps = FunderMapsSDK(db_config=db_config, s3_config=s3_config)
-
-        async def run_tasks():
-            try:
-                for task_name, task_func in task_registry.items():
-                    logger.debug(f"Running task '{task_name}'")
-                    await task_func(fundermaps)
-            finally:
-                for task_name, task_func in task_registry_post.items():
-                    logger.debug(f"Running post task '{task_name}'")
-                    await task_func(fundermaps)
-
-        logger.info("Starting 'map_gen'")
-        asyncio.run(run_tasks())
-        logger.info("Finished 'map_gen'")
-
-    except Exception as e:
-        logger.error("An error occurred", exc_info=e)
-        sys.exit(1)
+    util.remove_files(".", extension=".gpkg")
+    util.remove_files(".", extension=".geojson")
+    util.remove_files(".", extension=".mbtiles")
