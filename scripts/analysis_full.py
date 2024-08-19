4ad5c59a1e311f446b4ee2abdb4f25549ec59485
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-08-19 13:39:54 +0200

    Add script for full analysis data download and upload to S3

---
+++ scripts/analysis_full.py
@@ -0,0 +1,34 @@
+import logging
+
+from fundermapssdk import FunderMapsSDK
+from fundermapssdk import app
+
+
+BUCKET: str = "fundermaps"
+FILE_NAME: str = "analysis_full.gpkg"
+
+logger = logging.getLogger("analysis_full")
+
+
+@app.fundermaps_task
+async def run(fundermaps: FunderMapsSDK):
+    logger.info(f"Dowloading data")
+    await fundermaps.gdal.convert(
+        "PG:dbname=fundermaps",
+        FILE_NAME,
+        "maplayer.analysis_full",
+    )
+
+    with fundermaps.s3 as s3:
+        from datetime import datetime
+
+        current_date = datetime.now()
+        formatted_date_year = current_date.strftime("%Y")
+        formatted_date_month = current_date.strftime("%b").lower()
+
+        logger.info(f"Uploading {FILE_NAME} to S3")
+        await s3.upload_file(
+            BUCKET,
+            FILE_NAME,
+            f"model/{formatted_date_year}/{formatted_date_month}/{FILE_NAME}",
+        )
