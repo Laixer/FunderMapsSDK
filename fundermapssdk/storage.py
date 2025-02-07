1c071447dd615fc27f677e37571ab3d336c1f2df
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-02-07 12:52:30 +0000

    Add async upload_file2 method to ObjectStorageProvider for file uploads

--- fundermapssdk/storage.py
+++ fundermapssdk/storage.py
@@ -40,6 +40,37 @@ class ObjectStorageProvider:
 
         self.__logger(logging.DEBUG, f"File uploaded to {key}")
 
+    async def upload_file2(self, file_path: str, key: str, *args):
+        """
+        Uploads a file to the specified key in the storage bucket.
+
+        Args:
+            bucket (str): The name of the bucket to upload the file to.
+            file_path (str): The path of the file to be uploaded.
+            key (str): The key under which to store the file in the bucket.
+            *args: Additional arguments to be passed to the upload_file method.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        self.__logger(logging.DEBUG, f"Uploading file {file_path} to {key}")
+
+        config = TransferConfig(
+            multipart_threshold=1024 * 25,
+            max_concurrency=10,
+            multipart_chunksize=1024 * 25,
+            use_threads=True,
+        )
+
+        self.client.upload_file(
+            file_path, self.config.bucket, key, *args, Config=config
+        )
+
+        self.__logger(logging.DEBUG, f"File uploaded to {key}")
+
     def __enter__(self):
         self.__logger(logging.DEBUG, "Connecting to S3")
 
