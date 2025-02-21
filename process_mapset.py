367cdd5d2d5c4d3aac2ab52b13668f799187e04c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-02-21 11:27:13 +0000

    Refactor process_mapset to use TileBundle dataclass and streamline dataset processing logic

--- process_mapset.py
+++ process_mapset.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import tempfile
+from dataclasses import dataclass
 
 from prefect import flow, task
 from prefect.logging import get_run_logger
@@ -11,48 +12,40 @@ from fundermapssdk.config import DatabaseConfig, S3Config
 from fundermapssdk.tippecanoe import tippecanoe
 
 
+# TODO: Find a suitable name for the flow
+# - Mapset, TileSet, TileBundle, MapBundle, MapTile, MapTileset
+
+
+@dataclass
 class TileBundle:
-    def __init__(
-        self,
-        tileset: str,
-        min_zoom: int,
-        max_zoom: int,
-        upload_dataset: bool = False,
-        generate_tiles: bool = True,
-    ):
-        self.tileset = tileset
-        self.min_zoom = min_zoom
-        self.max_zoom = max_zoom
-        self.upload_dataset = upload_dataset
-        self.generate_tiles = generate_tiles
+    tileset: str
+    min_zoom: int
+    max_zoom: int
+    upload_dataset: bool = False
+    generate_tiles: bool = True
 
     def __str__(self):
-        return f"{self.name} ({self.tileset})"
+        return f"{self.tileset} ({self.tileset})"
 
 
 # TODO: Get from the database
-BUCKET: str = "fundermaps-tileset"
-BUNDLES: list[TileBundle] = [
-    TileBundle("analysis_foundation", 12, 16),
-    TileBundle("analysis_report", 12, 16),
-    TileBundle("analysis_building", 12, 16),
-    TileBundle("analysis_risk", 12, 16),
-    TileBundle("analysis_monitoring", 12, 16),
-    TileBundle("facade_scan", 12, 16, upload_dataset=True),
-    TileBundle("incident", 12, 16, upload_dataset=True),
-    TileBundle("incident_neighborhood", 10, 16, upload_dataset=True),
-    TileBundle("incident_municipality", 7, 11, upload_dataset=True),
-    TileBundle("incident_district", 10, 16, upload_dataset=True),
-    #
-    # TileBundle("building_cluster", 12, 16), # TODO: Missing tileset?
-    # TileBundle("building_supercluster", 12, 16), # TODO: Missing tileset?
-    #
-    # TileBundle("boundry_municipality", 7, 11),
-    # TileBundle("boundry_district", 10, 16),
-    # TileBundle("boundry_neighborhood", 10, 16),
-    #
-    TileBundle("analysis_full", 10, 16, upload_dataset=True, generate_tiles=False),
-]
+# TODO: Dont need this, configure via flow interface
+# BUCKET: str = "fundermaps-tileset"
+# BUNDLES: list[TileBundle] = [
+#     TileBundle("analysis_foundation", 12, 16),
+#     TileBundle("analysis_report", 12, 16),
+#     TileBundle("analysis_building", 12, 16),
+#     TileBundle("analysis_risk", 12, 16),
+#     TileBundle("analysis_monitoring", 12, 16),
+#     TileBundle("facade_scan", 12, 16, upload_dataset=True),
+#     TileBundle("incident", 12, 16, upload_dataset=True, generate_tiles=False),
+#     TileBundle(
+#         "incident_neighborhood", 10, 16, upload_dataset=True, generate_tiles=False
+#     ),
+#     TileBundle("incident_municipality", 7, 11, upload_dataset=True),
+#     TileBundle("incident_district", 10, 16, upload_dataset=True),
+#     TileBundle("analysis_full", 10, 16, upload_dataset=True, generate_tiles=False),
+# ]
 TILE_CACHE_MAX_AGE: int = 60 * 60 * 6  # 6 hours
 
 
@@ -94,15 +87,50 @@ def upload_dataset(fundermaps: FunderMapsSDK, tileset: TileBundle):
     with fundermaps.s3 as s3:
         logger.info(f"Uploading {tileset.tileset} to S3")
 
-        s3_path = f"model/{util.date_path()}/{tileset.tileset}.gpkg"
-        s3.upload_file(f"{tileset.tileset}.gpkg", s3_path)
+        s3_path = f"mapset/{util.date_path()}/{tileset.tileset}.gpkg"
+        s3.upload_file(f"{tileset.tileset}.gpkg", s3_path, bucket="fundermaps-data")
 
 
-@flow(name="Process Mapset")
-async def process_mapset():
+@task(name="Process Mapset", cache_policy=NO_CACHE)
+async def process_mapset(fundermaps: FunderMapsSDK, tileset: TileBundle):
+    logger = get_run_logger()
+
+    with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmp_dir:
+        os.chdir(tmp_dir)
+
+        await download_dataset(fundermaps, tileset)
+
+        if tileset.upload_dataset:
+            upload_dataset(fundermaps, tileset)
+
+        if tileset.generate_tiles:
+            await generate_tileset(fundermaps, tileset)
+
+            # TODO: Move everything to a separate task
+            logger.info(f"Uploading {tileset.tileset} to S3")
+
+            tile_files = util.collect_files_with_extension(tileset.tileset, ".pbf")
+
+            with fundermaps.s3 as s3:
+                tile_headers = {
+                    "CacheControl": f"max-age={TILE_CACHE_MAX_AGE}",
+                    "ContentType": "application/x-protobuf",
+                    "ContentEncoding": "gzip",
+                    "ACL": "public-read",
+                }
+
+                s3.upload_bulk(
+                    tile_files, bucket="fundermaps-tileset", extra_args=tile_headers
+                )
+
+
+# @flow(name="Extract Dataset")
+@flow
+async def extract_mapset(tilebundle: list[TileBundle]):
     db_config = DatabaseConfig(
         database="fundermaps",
-        host="db-pg-ams3-0-do-user-871803-0.b.db.ondigitalocean.com",
+        # host="db-pg-ams3-0-do-user-871803-0.b.db.ondigitalocean.com",
+        host="private-db-pg-ams3-0-do-user-871803-0.b.db.ondigitalocean.com",
         user="fundermaps",
         password="AVNS_CtcfLEuVWqRXiK__gKt",
         port=25060,
@@ -119,33 +147,34 @@ async def process_mapset():
 
     fundermaps = FunderMapsSDK(db_config=db_config, s3_config=s3_config)
 
-    # TODO: Fetch the tileset names from the database
-    for tileset in BUNDLES:
+    for tileset in tilebundle:
         logger.info(f"Processing tileset '{tileset.tileset}'")
 
-        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmp_dir:
-            os.chdir(tmp_dir)
-
-            await download_dataset(fundermaps, tileset)
-
-            if tileset.upload_dataset:
-                upload_dataset(fundermaps, tileset)
-
-            if tileset.generate_tiles:
-                await generate_tileset(fundermaps, tileset)
-
-                tile_files = util.collect_files_with_extension(tileset.tileset, ".pbf")
-
-                with fundermaps.s3 as s3:
-                    tile_headers = {
-                        "CacheControl": f"max-age={TILE_CACHE_MAX_AGE}",
-                        "ContentType": "application/x-protobuf",
-                        "ContentEncoding": "gzip",
-                        "ACL": "public-read",
-                    }
-
-                    s3.upload_bulk(tile_files, extra_args=tile_headers)
+        await process_mapset(fundermaps, tileset)
 
 
 if __name__ == "__main__":
-    asyncio.run(process_mapset())
+    asyncio.run(
+        extract_mapset(
+            [
+                TileBundle("analysis_foundation", 12, 16),
+                TileBundle("analysis_report", 12, 16),
+                TileBundle("analysis_building", 12, 16),
+                TileBundle("analysis_risk", 12, 16),
+                TileBundle("analysis_monitoring", 12, 16),
+                TileBundle("facade_scan", 12, 16, upload_dataset=True),
+                TileBundle("incident", 12, 16, upload_dataset=True),
+                TileBundle(
+                    "incident_neighborhood",
+                    10,
+                    16,
+                    upload_dataset=True,
+                ),
+                TileBundle("incident_municipality", 7, 11, upload_dataset=True),
+                TileBundle("incident_district", 10, 16, upload_dataset=True),
+                # TileBundle(
+                #     "analysis_full", 10, 16, upload_dataset=True, generate_tiles=False
+                # ),
+            ]
+        )
+    )
