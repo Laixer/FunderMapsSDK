d5670763058ac07c2ee150d8821e162456795e5c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-02-21 11:27:18 +0000

    Fix SQL query in load_subsidence.sql to select velocity from the correct table and add NULL check

--- fundermapssdk/sql/load_subsidence.sql
+++ fundermapssdk/sql/load_subsidence.sql
@@ -1,5 +1,6 @@
 INSERT INTO "data".building_subsidence
-SELECT b.external_id, velocity
+SELECT b.external_id, p.velocity
 FROM public.panden AS p
 JOIN geocoder.building AS b ON b.external_id = 'NL.IMBAG.PAND.' || p.identificatie
+WHERE p.velocity is not NULL
 ON conflict ON CONSTRAINT building_subsidence_pkey DO nothing;
