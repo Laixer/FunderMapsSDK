2724eb0bf7d77f5213b5b0ffe6796d35a30b3fc5
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-02-22 10:31:03 +0000

    Add automation triggers for flow run events in create_deployment.py

--- create_deployment.py
+++ create_deployment.py
@@ -1,10 +1,14 @@
 from prefect import flow
-
+from prefect.automations import Automation
+from prefect.events import DeploymentEventTrigger
+from prefect.events.schemas.automations import EventTrigger
+from prefect.events.actions import CancelFlowRun
 
 SOURCE_REPO = "git@github.com:Laixer/FunderMapsSDK.git"
 WORK_POOL_NAME = "fm-worker-1"
 
 # TODO: Move into /scripts folder
+# TODO: Add Automations
 
 if __name__ == "__main__":
     flow.from_source(
@@ -28,7 +32,49 @@ if __name__ == "__main__":
         cron="05 20 * * *",
         print_next_steps=False,
         concurrency_limit=1,
+        triggers=[
+            DeploymentEventTrigger(
+                enabled=True,
+                match={"prefect.resource.id": "prefect.flow-run.*"},
+                expect=["prefect.flow-run.Completed", "prefect.flow-run.Cached"],
+                match_related={
+                    "prefect.resource.role": "flow",
+                    "prefect.resource.id": [
+                        "prefect.flow.1c11c5ba-8d78-4671-a288-c583feb86c32"
+                    ],
+                },
+                after=[],
+                for_each=["prefect.resource.id"],
+                posture="Reactive",
+                threshold=1,
+                within=0,
+            )
+        ],
     )
+
+    # {
+    #   "type": "event",
+    #   "match": {
+    #     "prefect.resource.id": "prefect.flow-run.*"
+    #   },
+    #   "match_related": {
+    #     "prefect.resource.role": "flow",
+    #     "prefect.resource.id": [
+    #       "prefect.flow.1c11c5ba-8d78-4671-a288-c583feb86c32"
+    #     ]
+    #   },
+    #   "after": [],
+    #   "expect": [
+    #     "prefect.flow-run.Completed",
+    #     "prefect.flow-run.Cached"
+    #   ],
+    #   "for_each": [
+    #     "prefect.resource.id"
+    #   ],
+    #   "posture": "Reactive",
+    #   "threshold": 1,
+    #   "within": 0
+    # }
 
     flow.from_source(
         source=SOURCE_REPO,
@@ -152,3 +198,29 @@ if __name__ == "__main__":
             ]
         },
     )
+
+    # automation = Automation(
+    #     name="export-last-mapset2",
+    #     trigger=EventTrigger(
+    #         expect={"animal.walked"},
+    #         match={
+    #             "genus": "Marmota",
+    #             "species": "monax",
+    #         },
+    #         posture="Reactive",
+    #         threshold=3,
+    #     ),
+    #     actions=[CancelFlowRun()],
+    # ).create()
+
+    # print(automation)
+    # name='woodchonk' description='' enabled=True trigger=EventTrigger(type='event', match=ResourceSpecification(__root__={'genus': 'Marmota', 'species': 'monax'}), match_related=ResourceSpecification(__root__={}), after=set(), expect={'animal.walked'}, for_each=set(), posture=Posture.Reactive, threshold=3, within=datetime.timedelta(seconds=10)) actions=[CancelFlowRun(type='cancel-flow-run')] actions_on_trigger=[] actions_on_resolve=[] owner_resource=None id=UUID('d641c552-775c-4dc6-a31e-541cb11137a6')
+
+    # reading the automation
+
+    # automation = Automation.read(id=automation.id)
+    # or
+    # automation = Automation.read(name="woodchonk")
+
+    # print(automation)
+    # name='woodchonk' description='' enabled=True trigger=EventTrigger(type='event', match=ResourceSpecification(__root__={'genus': 'Marmota', 'species': 'monax'}), match_related=ResourceSpecification(__root__={}), after=set(), expect={'animal.walked'}, for_each=set(), posture=Posture.Reactive, threshold=3, within=datetime.timedelta(seconds=10)) actions=[CancelFlowRun(type='cancel-flow-run')] actions_on_trigger=[] actions_on_resolve=[] owner_resource=None id=UUID('d641c552-775c-4dc6-a31e-541cb11137a6')
